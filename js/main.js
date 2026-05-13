9f67e5d39a015b7d82f945367b73f1ab30ac6215
Gonzalo Silva <gonzalosilvapro@gmail.com>
2026-05-13 20:59:01 -0300

    agregamos funcionalidades

--- js/main.js
+++ js/main.js
@@ -8,3 +8,33 @@ async function init() {
 }
 
 init();
+
+let currentPage = 1;
+
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+async function loadCharacters(page) {
+  const data = await getCharacters(page);
+
+  renderCharacters(data.results);
+
+  document.getElementById(
+    "pageInfo"
+  ).textContent = `Página ${page} de ${data.info.pages}`;
+
+  prevBtn.disabled = !data.info.prev;
+  nextBtn.disabled = !data.info.next;
+}
+
+nextBtn.addEventListener("click", () => {
+  currentPage++;
+  loadCharacters(currentPage);
+});
+
+prevBtn.addEventListener("click", () => {
+  currentPage--;
+  loadCharacters(currentPage);
+});
+
+loadCharacters(currentPage);
